7c232e6a944b83fb960d4119ededd8e633d06c40
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2021-02-03 23:47:39 +0300

    Added entityframework.(3.02.2021)

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -10,7 +10,6 @@ namespace Entities.Concrete
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
-        public int MyProperty { get; set; }
         public short UnitsInStock { get; set; }
         public decimal UnitPrice { get; set; }
 
