b1a08253a0371e7b3d860f90e56f78c97e349272
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2021-02-11 00:02:05 +0300

    11.02.2021 Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+   public static class Messages
+    {
+        //public olanlar pascal keys yazılır normalde priv olan propertyi olanlar camel keys yazılır
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        public static string ProductsListed = "Ürünler listelendi.";
+        public static string MaintenanceTime = "Sistem bakımdadır.";
+    }
+}
