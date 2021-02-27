4fa5c8d383ea7372f85b71506f559cef345311e1
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2021-02-28 01:35:54 +0300

    27 şubat 3.ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -17,5 +19,12 @@ namespace Business.Constants
         public static  string ProductCountOfCategoryError="Category sınırı aşılmıştır.";
         public static string ProductNameAlreadyExists = "Bu isimde halihazırda isim vardır";
         public static string CategoryLimitExceded = "Category sınırı aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
     }
 }
